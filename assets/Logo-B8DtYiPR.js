1afaea870b7d92603a9e09f43d1ad11ca118fe6c
Nikita <personalnick@yandex.ru>
2025-06-23 01:26:50 +0300

    Updates

--- assets/Logo-B8DtYiPR.js
+++
@@ -1,1 +0,0 @@
-import{j as o,B as s,l as t}from"./index-YeBX6YoL.js";const a={logo:{height:44,width:"auto"}},g=()=>o.jsx(s,{component:"img",src:t,alt:"Эрудит",sx:a.logo,loading:"lazy"});export{g as default};
